f6571039e63bd496f7a70892db75278aff1ed4db
wkraz <wkrzaste@nd.edu>
2024-12-02 00:27:35 -0500

    add module 4

--- module-0/minitorch/__init__.py
+++ module-0/minitorch/__init__.py
@@ -1,6 +1,10 @@
 from .testing import MathTest, MathTestVariable  # type: ignore # noqa: F401,F403
-from .fast_ops import *  # noqa: F401,F403
-from .cuda_ops import *  # noqa: F401,F403
+from .datasets import *  # noqa: F401,F403
+from .optim import *  # noqa: F401,F403
+from .tensor import *  # noqa: F401,F403
+from .testing import *  # noqa: F401,F403
+from .nn import *  # noqa: F401,F403
+from .fast_conv import *  # noqa: F401,F403
 from .tensor_data import *  # noqa: F401,F403
 from .tensor_functions import *  # noqa: F401,F403
 from .tensor_ops import *  # noqa: F401,F403
@@ -15,4 +19,7 @@ from .tensor import *  # noqa: F401,F403
 from .datasets import *  # noqa: F401,F403
 from .testing import *  # noqa: F401,F403
 from .optim import *  # noqa: F401,F403
+from .tensor_ops import *  # noqa: F401,F403
+from .fast_ops import *  # noqa: F401,F403
+from .cuda_ops import *  # noqa: F401,F403
 from . import fast_ops, cuda_ops  # noqa: F401,F403
---
+++ module-0/minitorch/nn.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+
+from . import operators
+from .autodiff import Context
+from .fast_ops import FastOps
+from .tensor import Tensor
+from .tensor_functions import Function, rand, tensor
+
+
+# List of functions in this file:
+# - avgpool2d: Tiled average pooling 2D
+# - argmax: Compute the argmax as a 1-hot tensor
+# - Max: New Function for max operator
+# - max: Apply max reduction
+# - softmax: Compute the softmax as a tensor
+# - logsoftmax: Compute the log of the softmax as a tensor - See https://en.wikipedia.org/wiki/LogSumExp#log-sum-exp_trick_for_log-domain_calculations
+# - maxpool2d: Tiled max pooling 2D
+# - dropout: Dropout positions based on random noise, include an argument to turn off
+
+
+def tile(input: Tensor, kernel: Tuple[int, int]) -> Tuple[Tensor, int, int]:
+    """Reshape an image tensor for 2D pooling
+
+    Args:
+    ----
+        input: batch x channel x height x width
+        kernel: height x width of pooling
+
+    Returns:
+    -------
+        Tensor of size batch x channel x new_height x new_width x (kernel_height * kernel_width) as well as the new_height and new_width value.
+
+    """
+    batch, channel, height, width = input.shape
+    kh, kw = kernel
+    assert height % kh == 0
+    assert width % kw == 0
+    # TODO: Implement for Task 4.3.
+    raise NotImplementedError("Need to implement for Task 4.3")
+
+
+# TODO: Implement for Task 4.3.
